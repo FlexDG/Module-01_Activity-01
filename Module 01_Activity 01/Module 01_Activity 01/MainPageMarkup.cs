b81d184044c554c8a3b1f41f4554385eb8f6c985
Felix Rafael De Guzman <deguzman.felixrafael@auf.edu.ph>
2022-09-12 20:15:51 +0800

    first commit

---
+++ Module 01_Activity 01/Module 01_Activity 01/MainPageMarkup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module_01_Activity_01
+{
+   public class MainPageMarkup
+    {
+        public static readonly Color BtnClr = Color.White;
+        public static readonly Color BtnClrBg = Color.Black;
+        public static readonly Color RegBg = Color.DarkOrange;
+        public static readonly Color RegBg2 = Color.Green;
+        public static readonly Color RegBg3 = Color.DeepSkyBlue;
+        public static readonly Color RegBg4 = Color.Yellow;
+        public static readonly Color BgClr = Color.Red;
+    }
+}
